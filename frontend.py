6711657009ff78fcdbdb421a17fdb0a5cb8fb443
IshaanSohi <79596209+IshaanSohi@users.noreply.github.com>
2023-02-12 13:20:35 +0530

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,10 +1,4 @@
 flag=0
-import os
-folder = os.getcwd()
-for extension in ('.mp3', '.mp4'):
-    if((file)!=0):
-        for file in glob.glob(os.path.join(folder, f'*{extension}')):
-            os.remove(file)
 import streamlit as st
 import os
 st.title('Mashup')
